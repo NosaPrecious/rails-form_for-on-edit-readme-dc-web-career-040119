c070e621cd09c793554412b75a28f14acc84c37d
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-07 03:37:17 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(strong_params(:title, :description))
+	  @post.update(strong_params(:title)
 	  redirect_to post_path(@post)
 	end
 	
